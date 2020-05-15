412aafc643fb7978cfdec9d578f40c4692ee8352
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-15 02:15:51 -0700

    get recipe by id

--- api/recipes/recipeModel.js
+++ api/recipes/recipeModel.js
@@ -2,8 +2,16 @@ const db = require('../../data/dbConfig')
 
 module.exports = {
     getRecipes,
+    getRecipeById,
+
 }
 
+// returns contents of recipes table
 function getRecipes() {
     return db('recipes')
+}
+
+// returns recipe with a certain id
+function getRecipeById(id) {
+    return db('recipes').where({ id }).first()
 }
--- api/recipes/recipeRouter.js
+++ api/recipes/recipeRouter.js
@@ -10,8 +10,15 @@ router.get('/', (req, res) => {
             res.status(201).json(recipes)
         })
         .catch(() => {
-            res.status(500).json({ message: "Recipes could not be retrieved." })
+            res.status(500).json({ message: 'Recipes could not be retrieved.' })
         })
+})
+
+// returns an object containing the info for a recipe of a certain id
+router.get('/:id', (req, res) => {
+    Recipes.getRecipeById(req.params.id)
+        .then(recipe => {res.status(201).json(recipe)})
+        .catch(() => {res.status(500).json({ message: `Recipe with an id of ${req.params.id} could not be retrieved`})})
 })
 
 module.exports = router
